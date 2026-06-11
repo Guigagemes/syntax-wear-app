e65ea2b497db3062078327da88861b2e4d832547
Guilherme <guiga6157@gmail.com>
2026-06-11 12:27:32 -0300

    feat: adicina componente do header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Header } from "./componentes/Header"
 
 function App() {
   
 
   return (
-    <><p className="text-error">Hola Mundo</p></>
+    <>
+     <Header></Header>
+    </>
   )
 }
 
