c5a070e1c2f760c5fa86727bcafc2bc6eb780950
Guilherme <guiga6157@gmail.com>
2026-07-10 11:38:05 -0300

    feat: adicionada página de sobre

--- src/componentes/Header/index.tsx
+++ src/componentes/Header/index.tsx
@@ -30,7 +30,7 @@ export const Header = () => {
                                 <Link to='/our-stores'>Nossas lojas</Link>
                             </li>
                             <li className='hidden md:block'>
-                                <a href="#">Sobre</a>
+                                <Link to='/about'>Sobre</Link>
                             </li>
                             <li className='hidden md:block'>
                                 <a href="#"><img src={IconUser} alt="Icone de login" /></a>
